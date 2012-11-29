5a0756f4e6240a0e304041fbb5fb4eabdbf189e7
Pieter Breed <pieter.breed@allangray.co.za>
2012-11-29 16:00:18 +0200

    fiddling with namespace resolution on message members

--- src/parsering/massage.clj
+++ src/parsering/massage.clj
@@ -1,5 +1,6 @@
 (ns parsering.massage
-  (:use [parsering.lexer :only [split-namespace-elements parse-proto-file]]))
+  (:use [parsering.lexer :only [split-namespace-elements parse-proto-file]]
+        [parsering.common]))
 
 (defn extract-options
   "Extracts the options from a proto-file record and puts it in a map"
@@ -35,14 +36,58 @@
                          (apply concat))]
       (concat (map #(dissoc % :nesteds :enums) these-msgs) embeddeds))))
 
+(defn resolve-symbol-names
+  "Goes through a message, finds symbol references in the message members and tries to resolve them using the nearest symbol in the name space"
+  [message-record ns-symbols]
+  (let [rmsgns (->> (:full-name message-record) ;; reverse message namespace elements
+                    split-namespace-elements
+                    reverse)
+        limit (count rmsgns)]
+    (letfn [(test-symbol [name]
+              (contains? ns-symbols name))
+            (qualify-symbol [member-record]
+              (println "\n")
+              (let [member-parts (-> (:member-type member-record)
+                                     split-namespace-elements
+                                     reverse)]
+                (loop [i 0]
+                  (let [tested (->> (concat member-parts
+                                            (take i rmsgns))
+                                    reverse
+                                    (reduce #(str %1 "." %2)))]
+                    (println (str "testing: " tested))
+                    (cond
+                     (test-symbol tested) (assoc member-record :full-name tested)
+                     (< limit i) (assoc member-record :full-name "UNKNOWN")
+                     :else (recur (inc i)))))))
+            (update-symbol-ref [member-record]
+              (if (:member-is-simple-type member-record)
+                member-record
+                (qualify-symbol member-record)))]
+      (->> (:message-members message-record)
+           (map update-symbol-ref)
+           (assoc message-record :message-members)))))
+
 (defn load-proto-file
-  "Loads a protobuf file, resolves imports, does massaging on the file structure to make it into something that can be used for code-gen"
+  "Loads a protobuf file, resolves imports, does massaging on the file structure to make it into something that can be used for code-gen, returns a map with keys as fully qualified names and values maps with definitions"
   [filename resolver]
-  (-> (lex/parse-proto-file filename resolver)
-      (
+  (->> (parse-proto-file filename resolver)
+       (map #(let [{:keys [meta messages]} (split-headers-and-messages (:contents %))
+                   package (:package %)]
+               {:meta meta
+                :messages messages
+                :package package}))
+       (map #(assoc %
+               :options (extract-options (:meta %))))
+       (map #(let [{:keys [messages options package]} %]
+               (flatten-declarations messages package options)))
+       (apply concat '())
+       (reduce #(assoc %1 (:full-name %2) %2) {})))
+
+
 
 ;; lexer.clj
 ;; (def fsr (parsering.core/create-fs-resolver "resources"))
-;; 
+;; (-> (parsering.lexer/parse-proto-file "test2.proto" fsr) first :contents split-headers-and-messages pprint)
 
 

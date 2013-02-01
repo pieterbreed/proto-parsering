785378aebf8aec16b44f3940c4899d9b591fcdc0
Pieter Breed <pieter.breed@allangray.co.za>
2013-02-01 08:26:52 +0200

    fixed some of the tests

--- test/proto/decoder2_tests.clj
+++ test/proto/decoder2_tests.clj
@@ -5,18 +5,13 @@
         [the.parsatron]
         [debug.debug]))
 
-(defn make-bytes
-  "maps uncheked-byte on the input seq"
-  [xs]
-  (map unchecked-byte xs))
-
 (defmacro defunzip-val-test
   "tests an unzipping operation that results in a single value"
   [name testing operation & rest]
   `(deftest ~name
      (testing ~testing
        (are [seq#] (let [[res# bs#] seq#]
-                    (= res# (run (~operation) (make-bytes bs#))))
+                    (= res# (run (~operation) bs#)))
             ~@rest))))
 
 (defmacro defunzip-vec-test
@@ -25,7 +20,7 @@
   `(deftest ~name
      (testing ~testing
        (are [seq#] (let [[res# bs#] seq#]
-                    (= res# (vec (run (~operation) (make-bytes bs#)))))
+                    (= res# (vec (run (~operation) bs#))))
             ~@rest))))
 
 
@@ -34,7 +29,8 @@
  "happy cases: varints"
  varint
  [1 [1]]
- [300 [172 2]])
+ [300 [172 2]]
+ [-753 [0x8f,0xfa,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x1]])
 
 (defunzip-vec-test
   length-delimited-works2
@@ -47,7 +43,8 @@
   "that constant-size 64-bit values works"
   fixed-64
   [1 [1 0 0 0 0 0 0 0]]
-  [300 [44 1 0 0 0 0 0 0]])
+  [300 [44 1 0 0 0 0 0 0]]
+  [-4565996203501588736 [0x0,0x6f,0x81,0x4,0xa5,0x52,0xa2,0xc0]])
 
 (defunzip-val-test thirty-two-byte-works
   "that constant-size 32-bit values works"
@@ -71,19 +68,5 @@
   [{:wire-type :varint
     :tag-nr 1
     :value 150} [0x08 0x96 0x01]])
-
-;; Kom ons bou 'n 'SimpleTypesMsg'!
-;; signed int: -12
-;; double :32.23
-;; small int :22
-;; string :this is a string
-;; signed big int: -33344455566
-
-;; Result: [ 8 -12 -1 -1 -1 -1 -1 -1 -1 -1 1 16 44 25 61 10 -41 -93 112 29 64 64 34 16 116 104 105 115 32 105 115 32 97 32 115 116 114 105 110 103 40 -101 -114 -32 -73 -8 1] 
-
-;; (deftest real-simple-message-decode-test
-;;   (testing "we're deserializing a real message here encoded using google protobufs encoder"
-    
-
 
 (with-debugging (run-all-tests #"proto.decoder2-tests"))
